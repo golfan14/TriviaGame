75c5f2373390a8e50fd23e9e7e5ca6918f8e4bde
Stephen Ermisch <stephenermisch@Stephens-MacBook-Pro.local>
2018-05-02 21:25:37 -0600

    html

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -36,53 +36,46 @@ $(document).ready(function() {
             }
         }
     }
-
-
-
-
-
-
-
-
-
+}        
     
-    var Q1 = {
-        statement : 'Some animals can get sunburn',
-        answer : true
-    };
+var Q1 = {
+    statement : 'Some animals can get sunburn',
+    answer : true
+};
 
-    var Q2 = {
-        statement : 'No cat likes water',
-        answer : false
-    };
+var Q2 = {
+    statement : 'No cat likes water',
+    answer : false
+};
 
-    var Q3 = {
-        statement : 'Frogs have to drink lots of water',
-        answer : false
-    };
+var Q3 = {
+    statement : 'Frogs have to drink lots of water',
+    answer : false
+};
 
-    var Q4 = {
-        statement : 'Elephants eat with their noses',
-        answer : true
-    };
+var Q4 = {
+    statement : 'Elephants eat with their noses',
+    answer : true
+};
 
-    var Q5 = {
-        statement : 'Camels can walk 100 miles in the desert without water',
-        answer : true
-    };
+var Q5 = {
+    statement : 'Camels can walk 100 miles in the desert without water',
+    answer : true
+};
 
-    var Q6 = {
-        statement : 'Chimpanzees are nocturnal',
-        answer : false
-    };
+var Q6 = {
+    statement : 'Chimpanzees are nocturnal',
+    answer : false
+};
 
-    var questionArray = [Q1, Q2, Q3, Q4, Q5, Q6];
+var questionArray = [Q1, Q2, Q3, Q4, Q5, Q6];
+var currentQuestion = questionArray[i];
 
-    var index = 0;
-    var correct = 0;
-    var wrong = 0;
+var correct = 0;
+var wrong = 0;
 
-    
-
-
+function nextQuestion() {
+    timer.reset();
+    $("#question").html("<h3>" + questionArray)
+}
 
